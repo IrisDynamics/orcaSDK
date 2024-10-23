2b8c7a0ef81429302e076c639cd71aee17bd1821
Aiden Bull <aidenbull@hotmail.com>
2024-10-23 16:32:57 -0700

    [TESTING] Replaced run_in and run_out with run

--- tests/UnitAndIntegration/IrisSDKTests/modbus/IrisStreamHandshakeTests.cpp
+++ tests/UnitAndIntegration/IrisSDKTests/modbus/IrisStreamHandshakeTests.cpp
@@ -30,16 +30,16 @@ protected:
 	{
 		serial_interface->sendBuffer.clear();
 		serial_interface->consume_new_message(message_to_receive);
-		modbus_app.run_in();
+		modbus_app.run();
 		clock->pass_time(2001); //Interframe delay is 2000us
 
-		modbus_app.run_out();
+		modbus_app.run();
 	}
 };
 
 TEST_F(IrisStreamHandshakeTests, FirstHandshakeMessageIsPing)
 {
-	modbus_app.run_out();
+	modbus_app.run();
 	std::vector<char> output{ '\x1', '\b', '\0', '\0', '\x80', '\x1a'};
 	ASSERT_EQ(output, serial_interface->sendBuffer);
 }
@@ -48,15 +48,15 @@ TEST_F(IrisStreamHandshakeTests, FirstHandshakeMessageIsPing)
 // Not certain the value of this
 TEST_F(IrisStreamHandshakeTests, SecondHandshakeDoesntSendPing) 
 {
-	modbus_app.run_out();
+	modbus_app.run();
 	serial_interface->sendBuffer.clear();
-	modbus_app.run_out();
+	modbus_app.run();
 	ASSERT_EQ(std::vector<char>{}, serial_interface->sendBuffer);
 }
 
 TEST_F(IrisStreamHandshakeTests, SecondHandshakePingsAgainIfReceivedEcho)
 {
-	modbus_app.run_out();
+	modbus_app.run();
 	serial_interface->sendBuffer.clear();
 
 	ReceiveMessageAndSendResponse({ '\x1', '\b', '\0', '\0', '\x80', '\x1a' });

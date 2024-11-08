cb4c8f89f372d5d5b32cfbf7338386ed69bd7f62
Aiden Bull <aidenbull@hotmail.com>
2024-11-08 15:17:13 -0800

    [TESTING] Added auto-pass-time feature to test clock

--- tests/UnitAndIntegration/IrisSDKTests/modbus/helpers/TestClock.h
+++ tests/UnitAndIntegration/IrisSDKTests/modbus/helpers/TestClock.h
@@ -7,14 +7,21 @@ class TestClock : public Clock
 public:
 	int64_t get_time_microseconds() override 
 	{
+		pass_time(auto_time_pass);
 		return current_time;
 	}
 
-	void pass_time(uint32_t new_time)
+	void pass_time(int64_t new_time)
 	{
 		current_time += new_time;
 	}
 
+	void set_auto_time_pass(int64_t auto_time_pass)
+	{
+		this->auto_time_pass = auto_time_pass;
+	}
+
 private:
 	int64_t current_time = 0;
+	int64_t auto_time_pass = 0;
 };

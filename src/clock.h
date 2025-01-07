e5033e409f0e0961b0540a096bcac0bce8e63043
Aiden Bull <aidenbull@hotmail.com>
2025-01-06 16:23:26 -0800

    [DOCUMENTATION] Added comments to clock.h to describe implementation requirements

--- src/clock.h
+++ src/clock.h
@@ -2,8 +2,20 @@
 
 #include <cstdint>
 
+/** 
+ * @brief	An interface class for implementing time on non-standard systems
+ */
 class Clock
 {
 public:
+	/**
+	 * @brief	Obtain a timestamp for the current time in microseconds.
+	 * @note	The zero value for this time can be anything so long as 
+	 *			the clock increases roughly monotonically.
+	 * @note	The possible range of times must span the full signed 64-bit
+	 *			range. If the actual clock used for input to this function uses
+	 *			a more restricted data-type (eg 32-bit ints), then such input
+	 *			must be transformed to fit this range before being returned.
+	 */
 	virtual int64_t get_time_microseconds() = 0;
 };

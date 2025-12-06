7f428692fca1de53c7541b9c9a55c58d4fd4bc89
Aiden Bull <aidenbull@hotmail.com>
2025-12-05 16:51:01 -0800

    Updated orcaSDK CMake. Added dependency on new orca-api repo
    Tested using orcaSDK via CMake for the following use cases:
    - FetchContent orcaSDK and automatically download all dependencies
    - FetchContent orcaSDK and supply any of your own dependencies
    - Install orcaSDK with automatically downloaded dependencies
    - Install orcaSDK with manually supplied dependencies
    - find_package orcaSDK after following any install path

--- ASIO.cmake
+++ ASIO.cmake
@@ -1,23 +1,50 @@
-include(FetchContent)
-FetchContent_Declare(
-	asio-repo
-	GIT_REPOSITORY https://github.com/chriskohlhoff/asio.git
-	GIT_TAG 03ae834edbace31a96157b89bf50e5ee464e5ef9 # release 1.32.0
-	FIND_PACKAGE_ARGS
-)
+if(orcaSDK_VENDOR_ASIO)
+	project(asio)
 
-FetchContent_MakeAvailable(asio-repo)
+	include(GNUInstallDirs)
 
-file(GLOB_RECURSE asioHeaders_glob
-		"${asio-repo_SOURCE_DIR}/asio/include/*.hpp"
-		"${asio-repo_SOURCE_DIR}/asio/include/*.h"
-		"${asio-repo_SOURCE_DIR}/asio/include/*.ipp")
+	include(FetchContent)
+	FetchContent_Declare(
+		asio
+		GIT_REPOSITORY https://github.com/chriskohlhoff/asio.git
+		GIT_TAG 03ae834edbace31a96157b89bf50e5ee464e5ef9 # release 1.32.0
+	)
 
-target_sources(orcaSDK_core
-	PUBLIC
+	FetchContent_MakeAvailable(asio)
+
+	file(GLOB_RECURSE asioHeaders_glob
+			"${asio_SOURCE_DIR}/asio/include/*.hpp"
+			"${asio_SOURCE_DIR}/asio/include/*.h"
+			"${asio_SOURCE_DIR}/asio/include/*.ipp")
+
+	add_library(asio_headers INTERFACE)
+	add_library(asio::asio ALIAS asio_headers)
+
+	target_sources(asio_headers
+		PUBLIC
+			FILE_SET asioHeaders
+			TYPE HEADERS
+			BASE_DIRS "${asio_SOURCE_DIR}/asio/include"
+			FILES
+				${asioHeaders_glob}
+	)
+
+	set_target_properties(asio_headers
+		PROPERTIES
+			EXPORT_NAME asio
+	)
+	
+	install(TARGETS asio_headers
+		EXPORT asioConfig
 		FILE_SET asioHeaders
-		TYPE HEADERS
-		BASE_DIRS "${asio-repo_SOURCE_DIR}/asio/include"
-		FILES
-			${asioHeaders_glob}
-)
+	)
+	
+	install(EXPORT asioConfig
+		DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/asio
+		NAMESPACE asio::
+	)
+
+else()
+	find_package(asio)
+endif()
+target_link_libraries(orcaSDK_core PUBLIC asio::asio)
